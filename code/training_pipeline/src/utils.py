c75ddcd413633aa1286823717e7bc53fea98ba49
Thang Phan <thang.phancao@aalto.fi>
2023-07-18 17:55:16 +0300

    Reformat utils.py and make changes to RunInfo and EvaluationResult

--- code/training_pipeline/src/utils.py
+++ code/training_pipeline/src/utils.py
@@ -9,6 +9,7 @@ import pandas as pd
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
@@ -20,7 +21,7 @@ class AppConst:
     MODEL_TRAINING = "model_training"
     MODEL_EVALUATION = "model_evaluation"
     MLFLOW_MODEL_PATH_PREFIX = "model"
-    
+
 
 class AppPath:
     ROOT_DIR = os.getenv("ROOT_DIR")
@@ -30,7 +31,7 @@ class AppPath:
     DATA_SOURCE_DIR = Path(DATA_PIPELINE_DIR, "data_sources")
     ARTIFACTS_DIR = Path(TRAINING_PIPELINE_DIR, "artifacts")
     FEATURE_STORE_REPO = Path(DATA_PIPELINE_DIR, "feature_repo")
-    
+
     TRAINING_PQ = Path(ARTIFACTS_DIR, "train_data.parquet")
     TRAIN_X_PQ = Path(ARTIFACTS_DIR, "train_x.parquet")
     TRAIN_Y_PQ = Path(ARTIFACTS_DIR, "train_y.parquet")
@@ -39,15 +40,13 @@ class AppPath:
 
     RUN_INFO = Path(ARTIFACTS_DIR, "run_info.json")
     EVALUATION_RESULT = Path(ARTIFACTS_DIR, "evaluation.json")
-    
+
     def __init__(self) -> None:
         AppPath.ARTIFACTS_DIR.mkdir(parents=True, exist_ok=True)
-        
-        
+
+
 class Config:
     def __init__(self) -> None:
-        import numpy as np
-        
         self.feature_dict = {
             "area": np.float64,
             "width": np.float64,
@@ -56,100 +55,86 @@ class Config:
             "num_bathrooms": np.float64,
             "district": object,
             "city": object,
-            "legal_document": pd.CategoricalDtype(categories=['Giấy tờ hợp lệ',
-                                                              'Sổ hồng',
-                                                              'Sổ đỏ',
-                                                              'Hợp đồng mua bán',
-                                                              'Không xác định',
-                                                              'Giấy tờ viết tay'],
-                                                  ordered=False),
-            "price": np.float64
+            "price": np.float64,
+            "legal_document": pd.CategoricalDtype(
+                categories=[
+                    'Giấy tờ hợp lệ',
+                    'Sổ hồng',
+                    'Sổ đỏ',
+                    'Hợp đồng mua bán',
+                    'Không xác định',
+                    'Giấy tờ viết tay'
+                ],
+                ordered=False
+            )
         }
-        
+
         self.random_seed = 12
-        self.test_size= 0.2
+        self.test_size = 0.2
         self.target_col = "price"
-        
+
         self.mlflow_tracking_uri = os.getenv("MLFLOW_TRACKING_URI")
         self.experiment_name = "real_estate"
-        
+
+
 class Log:
     log: logging.Logger = None
 
     def __init__(self, name="") -> None:
-        if Log.log == None:
+        if Log.log is None:
             Log.log = self._init_logger(name)
 
     def _init_logger(self, name):
         logger = logging.getLogger(name)
-        formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        )
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
         stream_handler = logging.StreamHandler(sys.stdout)
         stream_handler.setFormatter(formatter)
         logger.addHandler(stream_handler)
         logger.setLevel(AppConst.LOG_LEVEL)
         return logger
-    
+
 
 class RunInfo:
-    def __init__(self, run_id) -> None:
+    def __init__(self, run_ids: list[str]) -> None:
         self.path = AppPath.RUN_INFO
-        self.run_id = run_id
-        
+        self.run_ids = run_ids
+
     def save(self):
-        run_info = {
-            "run_id": self.run_id
-        }
+        run_info = {"run_ids": self.run_ids}
         dump_json(run_info, self.path)
-            
+
+    def append(self, run_id):
+        self.run_ids.append(run_id)
+
     @staticmethod
     def load(path):
         data = load_json(path)
-        run_info = RunInfo(data["run_id"])
-        
+        run_info = RunInfo(data["run_ids"])
+
         return run_info
-    
-    
+
+
 class EvaluationResult:
-    def __init__(self, eval_metrics: dict) -> None:
+    def __init__(self, eval_results: dict) -> None:
         self.path = AppPath.EVALUATION_RESULT
-        self.eval_metrics = eval_metrics
-        
+        self.eval_results = eval_results
+
     def __str__(self) -> str:
         return str(self.__dict__)
-    
+
+    def add_eval_result(self, eval_result):
+        self.eval_results.update(eval_result)
+
     def save(self):
-        dump_json(self.eval_metrics, self.path)
-    
+        dump_json(self.eval_results, self.path)
+
     @staticmethod
     def load(path):
         data = load_json(path)
-        eval_result = EvaluationResult(
-            data
-        )
+        eval_result = EvaluationResult(data)
         return eval_result
-    
-class EvaluationResult:
-    def __init__(self, eval_metrics: dict) -> None:
-        self.path = AppPath.EVALUATION_RESULT
-        self.eval_metrics = eval_metrics
-        
-    def __str__(self) -> str:
-        return str(self.__dict__)
-    
-    def save(self):
-        dump_json(self.eval_metrics, self.path)
-    
-    @staticmethod
-    def load(path):
-        data = load_json(path)
-        eval_result = EvaluationResult(
-            data
-        )
-        return eval_result
-    
-    
+
+
 def inspect_dir(path):
     Log().log.info(f"Started: inspect_dir({path})")
     path = Path(path)
@@ -159,19 +144,19 @@ def inspect_dir(path):
     elif path.is_file():
         Log().log.info(f"Path {path} is file")
         return
-    
+
     paths = os.listdir(path)
     paths = sorted(paths)
     for path in paths:
         Log().log.info(path)
-    
-    
+
+
 def inspect_current_dir():
     cwd = os.getcwd()
     Log().log.info(f"Current directory: {cwd}")
     inspect_dir(cwd)
-    
-    
+
+
 def read_parquet(path) -> pd.DataFrame:
     Log().log.info(f"Stared: read_parquet {path}")
     df = pd.read_parquet(path, engine="fastparquet")
@@ -181,7 +166,7 @@ def read_parquet(path) -> pd.DataFrame:
 def to_parquet(df: pd.DataFrame, path):
     Log().log.info(f"Started: to_parquet {path}")
     df.to_parquet(path, engine="fastparquet")
-    
+
 
 def train_test_to_parquet(X_train, X_test, y_train, y_test):
     Log().log.info(f"Started: train_test_to_parquet")
@@ -189,16 +174,19 @@ def train_test_to_parquet(X_train, X_test, y_train, y_test):
     to_parquet(X_test, AppPath.TEST_X_PQ)
     to_parquet(y_train, AppPath.TRAIN_Y_PQ)
     to_parquet(y_test, AppPath.TEST_Y_PQ)
-    
+
+
 def dump_json(dict_obj: dict, path):
     with open(path, "w", encoding="utf-8") as f:
-        json.dump(dict_obj, f)
-        
+        json.dump(dict_obj, f, indent=4)
+
+
 def load_json(path) -> dict:
     with open(path, "r", encoding="utf-8") as f:
         data = json.load(f)
-    
+
     return data
+
 
 def evaluate_metrics(actual, predict, prefix="train") -> dict:
     Log().log.info(f"Stared: evaluate_metrics [{prefix}]")
@@ -206,11 +194,6 @@ def evaluate_metrics(actual, predict, prefix="train") -> dict:
     mae = mean_absolute_error(actual, predict)
     mse = mean_squared_error(actual, predict)
     rmse = np.sqrt(mse)
-    metrics = {
-        "r2_score": r2,
-        "mae": mae,
-        "mse": mse,
-        "rmse": rmse
-    }
+    metrics = {"r2_score": r2, "mae": mae, "mse": mse, "rmse": rmse}
     metrics = dict(zip([prefix + "_" + key for key in metrics.keys()], metrics.values()))
     return metrics
