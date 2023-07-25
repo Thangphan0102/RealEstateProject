1f5f9728ccb98bcc30946c9db4362431e1c6d96b
Thang Phan <thang.phancao@aalto.fi>
2023-07-25 14:14:54 +0300

    Add dev_requirements.txt and remove importing unecessary libraries

--- code/training_pipeline/src/model_training.py
+++ code/training_pipeline/src/model_training.py
@@ -1,3 +1,4 @@
+import logging
 import uuid
 import numpy as np
 from typing import TypedDict
@@ -11,7 +12,6 @@ from hyperopt import STATUS_OK
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.base import BaseEstimator
 from xgboost import XGBModel
-# from lightgbm import LGBMModel
 
 from utils import *
 
@@ -103,7 +103,7 @@ class SklearnTrainer(BaseTrainer):
     def __init__(self, model: BaseEstimator, params: TypedDict):
         super().__init__(model, params)
 
-    def train(self, **kwargs):
+    def train(self, train_x, train_y):
         with mlflow.start_run(nested=True, description="child-run") as run:
             # Train model
             self.trained_model = super().train(train_x, train_y)
@@ -152,51 +152,6 @@ class XGBTrainer(BaseTrainer):
         )
 
 
-# class LGBMTrainer(BaseTrainer):
-#     def __init__(self, model: LGBMModel, params: TypedDict) -> None:
-#         super().__init__(model, params)
-#
-#         self.model = model
-#         self.trained_model = None
-#         self.params = params
-#         self.model.set_params(**self.params)
-#
-#     def train(self, **kwargs):
-#         # Prepare data
-#         train_x, train_y = super().load_data()
-#
-#         # Train model
-#         self.trained_model = super().train(train_x, train_y)
-#
-#         # Evaluate train metrics
-#         pred_y = self.trained_model.predict(train_x)
-#         train_metrics = evaluate_metrics(train_y, pred_y, prefix="train")
-#
-#         # Log metadata
-#         signature = infer_signature(train_x, self.trained_model.predict(train_x))
-#         self.log_metadata(signature, train_metrics)
-#
-#         # End run
-#         mlflow.end_run()
-#
-#         # Save run_id to json
-#         run_id = mlflow.last_active_run().info.run_id
-#         self.save_run_id(run_id)
-#
-#         # Inspect metadata
-#         fetch_logged_data(run_id)
-#
-#     def log_metadata(self, signature: ModelSignature, train_metrics: dict) -> None:
-#         super().log_metadata(self.trained_model, train_metrics)
-#
-#         mlflow.log_params(self.trained_model.get_params(deep=True))
-#         mlflow.lightgbm.log_model(
-#             lgb_model=self.trained_model,
-#             artifact_path=AppConst.MLFLOW_MODEL_PATH_PREFIX,
-#             signature=signature
-#         )
-
-
 if __name__ == "__main__":
     from xgboost import XGBRegressor
     from hyperopt import fmin, tpe, Trials
@@ -221,7 +176,7 @@ if __name__ == "__main__":
             space=search_space,
             algo=tpe.suggest,
             trials=trials,
-            max_evals=50,
+            max_evals=1,
             rstate=np.random.default_rng(config.random_seed)
         )
 
