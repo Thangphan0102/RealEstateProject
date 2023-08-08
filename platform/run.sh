f924d0dded3c68c081fc645778f827b76adb4bb8
Thang Phan <thang.phancao@aalto.fi>
2023-08-08 20:07:29 +0300

    Add Prometheus and Grafana platforms

--- platform/run.sh
+++ platform/run.sh
@@ -4,16 +4,22 @@ service=$1  # Service as the first argument
 cmd=$2      # Command as the second argument
 
 # Service names
+ALL="all"
 MLFLOW="mlflow"
 AIRFLOW="airflow"
+ELK="elk"
+PROM_GRAF="prom-graf"
 RESTART_SLEEP_SEC=2
 
 usage() {
     echo "Usage:"
     echo "  run.sh <service> <command> [options]"
     echo "Available services:"
+    echo "  $ALL"
     echo "  $MLFLOW"
     echo "  $AIRFLOW"
+    echo "  $ELK"
+    echo "  $PROM_GRAF"
     echo "Available commands:"
     echo "  up           run container"
     echo "  down         stop and remove container"
@@ -69,6 +75,39 @@ down_airflow() {
     down "$AIRFLOW" "$@"
 }
 
+# ELK
+up_elk() {
+    docker-compose -f "$ELK/$ELK-docker-compose.yml" -f "$ELK/extensions/filebeat/filebeat-compose.yml" up -d "$@"
+}
+
+down_elk() {
+    docker-compose -f "$ELK/$ELK-docker-compose.yml" -f "$ELK/extensions/filebeat/filebeat-compose.yml" down "$@"
+}
+
+# PROM_GRAF
+up_prom_graf() {
+    up "$PROM_GRAF" "$@"
+}
+
+down_prom_graf() {
+    down "$PROM_GRAF" "$@"
+}
+
+# ALL
+up_all() {
+    up_elk "$@"
+    up_airflow "$@"
+    up_mlflow "$@"
+    up_prom_graf "$@"
+}
+
+down_all() {
+    down_elk "$@"
+    down_airflow "$@"
+    down_mlflow "$@"
+    down_prom_graf "$@"
+}
+
 if [[ "$1" == "-h" ]]; then
     usage
     exit 0
@@ -89,11 +128,20 @@ shift 2
 case $cmd in
     up)
         case $service in
+            "$ALL")
+                up_all "$@"
+                ;;
             "$MLFLOW")
                 up_mlflow "$@"
                 ;;
             "$AIRFLOW")
                 up_airflow "$@"
+                ;;
+            "$ELK")
+                up_elk "$@"
+                ;;
+            "$PROM_GRAF")
+                up_prom_graf "$@"
                 ;;
             *)
                 echo "Unknown service"
@@ -104,11 +152,20 @@ case $cmd in
         ;;
     down)
         case $service in
+            "$ALL")
+                down_all "$@"
+                ;;
             "$MLFLOW")
                 down_mlflow "$@"
                 ;;
             "$AIRFLOW")
                 down_airflow "$@"
+                ;;
+            "$ELK")
+                down_elk "$@"
+                ;;
+            "$PROM_GRAF")
+                down_prom_graf "$@"
                 ;;
             *)
                 echo "Unknown service"
@@ -119,6 +176,11 @@ case $cmd in
         ;;
     restart)
         case $service in
+            "$ALL")
+                down_all "$@"
+                sleep $RESTART_SLEEP_SEC
+                up_all "$@"
+                ;;
             "$MLFLOW")
                 down_mlflow "$@"
                 sleep $RESTART_SLEEP_SEC
@@ -128,6 +190,16 @@ case $cmd in
                 down_airflow "$@"
                 sleep $RESTART_SLEEP_SEC
                 up_airflow "$@"
+                ;;
+            "$ELK")
+                down_elk "$@"
+                sleep $RESTART_SLEEP_SEC
+                up_elk "$@"
+                ;;
+            "$PROM_GRAF")
+                down_prom_graf "$@"
+                sleep $RESTART_SLEEP_SEC
+                up_prom_graf "$@"
                 ;;
             *)
                 echo "Unknown service"
